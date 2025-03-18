0b3ab0eec048a86558c20effe4e433ee58716785
awatersny <awatersny@gmail.com>
2025-03-18 11:46:07 -0400

    Store image info in object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 const board = document.getElementById(board)
+let cells
+
+const pics = [
+  {
+    src: "../assets/atlantic_sunset.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/bk_bridge.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/pexels-alexgtacar-745150-1592384.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/pexels-luis-gomes-166706-546819.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/pexels-padrinan-1272838.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/pexels-pixabay-46148.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/pexels-pixabay-161043.jpg",
+    instances: 0
+  },
+  {
+    src: "../assets/pexels-pixabay-290386.jpg",
+    instances: 0
+  },
+]
